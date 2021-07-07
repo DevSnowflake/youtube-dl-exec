c9f13c01bca0301f9eac8f4a50631608aee4d9f0
Snowflake107 <46562212+Snowflake107@users.noreply.github.com>
2021-07-07 18:41:47 +0545

    chore(release): 1.0.2

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@
 const dargs = require('dargs')
 const execa = require('execa')
 
-const { YOUTUBE_DL_PATH } = require('./constants')
-
 const args = (url, flags = {}) =>
   [].concat(url, dargs(flags, { useEquals: false })).filter(Boolean)
 
@@ -12,7 +10,8 @@ const isJSON = str => str.startsWith('{')
 
 const parse = ({ stdout }) => (isJSON(stdout) ? JSON.parse(stdout) : stdout)
 
-const raw = (url, flags, opts) => execa(YOUTUBE_DL_PATH, args(url, flags), opts)
+const raw = (url, flags, opts) =>
+  execa(require('./constants').YOUTUBE_DL_PATH, args(url, flags), opts)
 
 module.exports = (url, flags, opts) => raw(url, flags, opts).then(parse)
 
