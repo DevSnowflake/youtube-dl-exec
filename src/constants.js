40f2d2754908ebcb68380672e8d9ecf3ce9b4b9f
Snowflake107 <46562212+Snowflake107@users.noreply.github.com>
2021-07-07 18:28:52 +0545

    refactor: make it nexusnode compatible

--- src/constants.js
+++ src/constants.js
@@ -6,28 +6,28 @@ const path = require('path')
 const PLATFORM_WIN = 'win32'
 const PLATFORM_UNIX = 'unix'
 
-const YOUTUBE_DL_HOST =
-  process.env.YOUTUBE_DL_HOST ||
-  'https://api.github.com/repos/ytdl-org/youtube-dl/releases?per_page=1'
-
-const YOUTUBE_DL_DIR =
-  process.env.YOUTUBE_DL_DIR || path.join(__dirname, '..', 'bin')
-
-const YOUTUBE_DL_PLATFORM =
-  process.env.YOUTUBE_DL_PLATFORM || isUnix(process.platform)
-    ? PLATFORM_UNIX
-    : PLATFORM_WIN
-
-const YOUTUBE_DL_FILENAME =
-  process.env.YOUTUBE_DL_FILENAME ||
-  `youtube-dl${YOUTUBE_DL_PLATFORM === PLATFORM_WIN ? '.exe' : ''}`
-
-const YOUTUBE_DL_PATH = path.join(YOUTUBE_DL_DIR, YOUTUBE_DL_FILENAME)
-
 module.exports = {
-  YOUTUBE_DL_DIR,
-  YOUTUBE_DL_FILENAME,
-  YOUTUBE_DL_HOST,
-  YOUTUBE_DL_PATH,
-  YOUTUBE_DL_PLATFORM
+  get YOUTUBE_DL_DIR () {
+    return process.env.YOUTUBE_DL_DIR || path.join(__dirname, '..', 'bin')
+  },
+  get YOUTUBE_DL_FILENAME () {
+    return (
+      process.env.YOUTUBE_DL_FILENAME ||
+      `youtube-dl${this.YOUTUBE_DL_PLATFORM === PLATFORM_WIN ? '.exe' : ''}`
+    )
+  },
+  get YOUTUBE_DL_HOST () {
+    return (
+      process.env.YOUTUBE_DL_HOST ||
+      'https://api.github.com/repos/ytdl-org/youtube-dl/releases?per_page=1'
+    )
+  },
+  get YOUTUBE_DL_PATH () {
+    return path.join(this.YOUTUBE_DL_DIR, this.YOUTUBE_DL_FILENAME)
+  },
+  get YOUTUBE_DL_PLATFORM () {
+    return process.env.YOUTUBE_DL_PLATFORM || isUnix(process.platform)
+      ? PLATFORM_UNIX
+      : PLATFORM_WIN
+  }
 }
